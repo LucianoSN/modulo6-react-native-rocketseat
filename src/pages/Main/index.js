8686559612a982f5509a3cd99ae67511cb149eae
Luciano de Souza Nery <lsnery@gmail.com>
2019-07-20 17:45:51 -0300

    adicionando axios e flatlist

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { Keyboard, Text } from 'react-native';
+import api from '../../services/api';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import { Container, Form, Input, SubmitButton, List } from './styles';
 
-export default function Main() {
-	return (
-		<Container>
-			<Form>
-				<Input
-					autoCorrect={false}
-					autoCapitalize="none"
-					placeholder="Adicionar usuário"
+export default class Main extends Component {
+	state = {
+		newUser: '',
+		users: [],
+	};
+
+	handleAddUser = async () => {
+		const { users, newUser } = this.state;
+		const response = await api.get(`/users/${newUser}`);
+
+		const data = {
+			name: response.data.name,
+			login: response.data.login,
+			bio: response.data.bio,
+			avatar: response.data.avatar_url,
+		};
+
+		this.setState({
+			users: [...users, data],
+			newUser: '',
+		});
+
+		Keyboard.dismiss();
+	};
+
+	render() {
+		const { newUser } = this.state;
+		return (
+			<Container>
+				<Form>
+					<Input
+						autoCorrect={false}
+						autoCapitalize="none"
+						placeholder="Adicionar usuário"
+						value={newUser}
+						onChangeText={text => this.setState({ newUser: text })}
+						returnKeyType="send"
+						onSubmitEditing={this.handleAddUser}
+					/>
+					<SubmitButton onPress={this.handleAddUser}>
+						<Icon name="add" size={20} color="#fff" />
+					</SubmitButton>
+				</Form>
+
+				<List
+					data={users}
+					keyExtractor={user => user.login}
+					renderItem={({ item }) => (
+						<User>
+							<Avatar source={{ uri: item.avatar }} />>
+							<Name>{item.name}</Name>
+							<Bio>{item.bio}</Bio>
+							<ProfileButton onPress={() => {}}>
+								<Text>Enviar</Text>
+							</ProfileButton>
+						</User>
+					)}
 				/>
-				<SubmitButton>
-					<Icon name="add" size={20} color="#fff" />
-				</SubmitButton>
-			</Form>
-		</Container>
-	);
+			</Container>
+		);
+	}
 }
 
 Main.navigationOptions = {
