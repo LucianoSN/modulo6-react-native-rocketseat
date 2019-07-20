907481cdebd6d24e68ec98e520f9fa2be5ad43c4
Luciano de Souza Nery <lsnery@gmail.com>
2019-07-20 15:08:10 -0300

    adicionado icones e estilização

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -13,13 +13,22 @@ export const Form = styled.View`
 	border-color: #eee;
 `;
 
-export const Input = styled.TextInput`
+export const Input = styled.TextInput.attrs({
+	placeholderTextColor: '#999',
+})`
 	flex: 1;
-	height: 40px;
+	height: 30px;
 	background: #eee;
 	border-radius: 4px;
 	padding: 0 15px;
 	border: 1px solid #eee;
 `;
 
-export const SubmitButton = styled(RectButton)``;
+export const SubmitButton = styled(RectButton)`
+	justify-content: center;
+	align-items: center;
+	background-color: #7159c1;
+	border-radius: 4px;
+	margin-left: 10px;
+	padding: 0 12px;
+`;
