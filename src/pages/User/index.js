44e1a090c62f792b26da2464a1c2ab4961294933
Luciano de Souza Nery <lsnery@gmail.com>
2019-07-20 22:37:01 -0300

    adicionado carregamento dos dados do usuário

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,7 +3,19 @@ import PropTypes from 'prop-types';
 import { View } from 'react-native';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+	Container,
+	Header,
+	Avatar,
+	Name,
+	Bio,
+	Star,
+	Starred,
+	OwnerAvatar,
+	Info,
+	Title,
+	Author,
+} from './styles';
 
 export default class User extends Component {
 	static navigationOptions = ({ navigation }) => ({
@@ -29,8 +41,35 @@ export default class User extends Component {
 	}
 
 	render() {
+		const { navigation } = this.props;
 		const { stars } = this.state;
 
-		return <View />;
+		const user = navigation.getParam('user');
+
+		return (
+			<Container>
+				<Header>
+					<Avatar source={{ uri: user.avatar }} />
+					<Name>{user.name}</Name>
+					<Bio>{user.bio}</Bio>
+				</Header>
+
+				<Star
+					data={stars}
+					keyExtractor={star => String(star.id)}
+					renderItem={({ item }) => (
+						<Starred>
+							<OwnerAvatar
+								source={{ uri: item.owner.avatar_url }}
+							/>
+							<Info>
+								<Title>{item.name}</Title>
+								<Author>{item.owner.login}</Author>
+							</Info>
+						</Starred>
+					)}
+				/>
+			</Container>
+		);
 	}
 }
